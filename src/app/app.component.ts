f1136c72131aa83d2b68d5d65c5b3486a7a73f0d
Maiki <ismenemaiki@gmail.com>
2020-12-02 10:52:57 -0300

    configuração jquery e bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { AfterViewChecked, Component } from '@angular/core';
 
+declare var $: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewChecked{
   title = 'sbt-angular';
+
+  ngAfterViewChecked() {
+    $('#click').on('click', () => {
+      alert('teste jquery');
+    });
+  }
 }
